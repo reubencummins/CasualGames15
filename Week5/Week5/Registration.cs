ed0eb40365a7721e4bcad3d9b72ad6298994c3d7
reubencummins <reubencummins@gmail.com>
2015-10-13 09:59:21 +0100

    Added user registration via console app

---
+++ Week5/Week5/Registration.cs
@@ -0,0 +1,10 @@
+﻿namespace Week5
+{
+    public class Registration
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}        
